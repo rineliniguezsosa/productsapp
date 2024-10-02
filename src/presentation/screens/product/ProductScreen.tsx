850e2c81fce632a437016905962accb0e54893f8
Rinel <rineliniguezsosa@gmail.com>
2024-10-01 20:09:39 -0600

    [FEAT] Creando el funcional component ProductScreen

---
+++ src/presentation/screens/product/ProductScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+
+export const ProductScreen = () => {
+  return (
+    <View>
+      <Text>ProductScreen</Text>
+    </View>
+  );
+};
