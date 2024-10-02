8bc13636d5c7755a4dbcae0ffd98618e217bc1e9
Rinel <rineliniguezsosa@gmail.com>
2024-10-01 20:30:03 -0600

    [FEAT] Creando el type  RootStackParams para tipar las rutas y params

---
+++ src/types/Interfaces.tsx
@@ -0,0 +1,7 @@
+export type RootStackParams = {
+    LoginScreen:undefined,
+    RegisterScreen:undefined,
+    HomeScreen:undefined,
+    LoadingScreen:undefined,
+    ProductScreen: { productId: number }
+};
