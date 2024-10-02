f5a47922bd7737337fbcb70d3db1d89f7cd952d0
Rinel <rineliniguezsosa@gmail.com>
2024-10-02 14:27:17 -0600

    [FEAT] Multiples cambios en el archivo RegisterScreen, estableciendo el design de la vista RegisterScreen

--- src/presentation/screens/auth/RegisterScreen.tsx
+++ src/presentation/screens/auth/RegisterScreen.tsx
@@ -1,10 +1,62 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { View,Text } from 'react-native';
+import { Button, Input, Layout,Text } from '@ui-kitten/components';
+import { ScrollView, useWindowDimensions } from 'react-native';
+import { MyIcon } from '../../../components/ui/MyIcon';
+import { StackScreenProps } from '@react-navigation/stack';
+import { RootStackParams } from '../../../types/Interfaces';
 
-export const RegisterScreen = () => {
+interface Props extends StackScreenProps<RootStackParams,'RegisterScreen'>{}
+
+export const RegisterScreen = ({navigation}:Props) => {
+  const { height } = useWindowDimensions();
   return (
-    <View>
-      <Text>RegisterScreen</Text>
-    </View>
+    <Layout style={{flex:1}}>
+      <ScrollView style={{marginHorizontal:40}}>
+
+        <Layout style={{paddingTop:height * 0.30}} >
+          <Text category="h1">Crear Cuenta</Text>
+          <Text category="label">Porfavor,Ingrese para continuar</Text>
+        </Layout>
+
+        <Layout style={{marginTop:20}}>
+
+          <Input
+            placeholder="Nombre completo"
+            style={{marginBottom:10}}
+            accessoryLeft={<MyIcon name="person-outline"/>}
+          />
+
+          <Input
+            placeholder="Email"
+            autoCapitalize="none"
+            keyboardType="email-address"
+            style={{marginBottom:10}}
+            accessoryLeft={<MyIcon name="email-outline"/>}
+          />
+
+          <Input
+            placeholder="Contraseña"
+            autoCapitalize="none"
+            secureTextEntry
+            style={{marginBottom:10}}
+            accessoryLeft={<MyIcon name="lock-outline"/>}
+          />
+
+        </Layout>
+
+        <Layout style={{marginTop:20}}>
+          <Button accessoryRight={<MyIcon name="arrow-forward-outline" white/>} onPress={()=> {}}>
+            Crear
+          </Button>
+        </Layout>
+
+        <Layout style={{marginTop:20,flexDirection:'row',justifyContent:'center',alignItems:'flex-end'}}>
+          <Text>¿Ya tienes una cuenta?</Text>
+          <Text status="primary" category="s1" onPress={()=> navigation.goBack()}>Ingresar</Text>
+        </Layout>
+
+      </ScrollView>
+    </Layout>
   );
 };
