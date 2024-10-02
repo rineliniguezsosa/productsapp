be898e73fb5f9806fd405aaf296d49ae70bce89e
Rinel <rineliniguezsosa@gmail.com>
2024-10-02 17:30:03 -0600

    [FEAT] Cambios en el archico TesloApi.ts, en este archivo defineremos la url principal para poder realizar solicitudes http
    - importando las variables de entorno
    - importando axios
    - importando la función Platform para validar el dispositivo en el que nos encontramos
    - creando la variable API_URL donde vamos a comprobar si estamos en modo desarrollo ó producción, si estamos en producción vamos a mostrar la url  PROD_URL después validaremos el tipo de plataforma en la que nos encontramos si estamos en android mostraremos la url API_URL_IOS y si no mostraremos la url API_URL_ANDROID
    - creamos la variable tesloApi para definir la url base para establecer la solicitudes http
    - exportamos la variable tesloApi para tener acceso en cualquier parte y realizar solicitudes http

---
+++ src/config/api/TesloApi.ts
@@ -0,0 +1,11 @@
+import { STAGE,API_URL as PROD_URL,API_URL_ANDROID,API_URL_IOS } from '@env';
+import axios from 'axios';
+import { Platform } from 'react-native';
+
+const API_URL = (STAGE === 'prod') ? PROD_URL : Platform.OS === 'ios' ? API_URL_IOS : API_URL_ANDROID;
+
+const tesloApi = axios.create({baseURL:API_URL,headers:{'Content-Type':'application/json'}});
+
+export {
+    tesloApi,
+};
