a6123c58704bcc4515395208a1ed7582148ff519
Rinel <rineliniguezsosa@gmail.com>
2024-10-01 20:41:51 -0600

    [FEAT] Multiples cambios en el archivo StackNavigator.tsx
    - importando React
    - importando createStackNavigator para la creación del stack
    - importando  RootStackParams para tipar las rutas del stack
    - importando todas los screens HomeScreen,LoginScreen,RegisterScreen,LoadingScreen,ProductScreen
    - creando la variable Stack donde pasamos la referencia de la función createStackNavigator para crear el stack
    - tipamos el stack navigator mediante el type RootStackParams
    - creamos el funcional component StackNavigator para la definción de las rutas del stack
    - estableciendo las rutas y vistas por devolver del stack navigator

---
+++ src/navigation/StackNavigator.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { RootStackParams } from '../types/Interfaces';
+import { HomeScreen,LoginScreen,RegisterScreen,LoadingScreen,ProductScreen } from '../presentation/screens';
+
+const Stack = createStackNavigator<RootStackParams>();
+
+export const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+    <Stack.Screen name="HomeScreen" component={HomeScreen} />
+    <Stack.Screen name="LoginScreen" component={LoginScreen} />
+    <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+    <Stack.Screen name="LoadingScreen" component={LoadingScreen} />
+    <Stack.Screen name="ProductScreen" component={ProductScreen} />
+  </Stack.Navigator>
+  );
+};
