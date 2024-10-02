e7b383f283d97eb8d5b0d7e5c982f008d0417bd9
Rinel <rineliniguezsosa@gmail.com>
2024-10-01 20:33:35 -0600

    [FEAT] Exportamos todos los archivos dentro de la carpeta screen dentro de nuestro archivo de barril index.ts

---
+++ src/presentation/screens/index.ts
@@ -0,0 +1,5 @@
+export * from './auth/LoginScreen';
+export * from './auth/RegisterScreen';
+export * from './home/HomeScreen';
+export * from './loading/LoadingScreen';
+export * from './product/ProductScreen';
