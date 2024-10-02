01cb3210074ce566f4366175e1b6d4f010f45a0d
Rinel <rineliniguezsosa@gmail.com>
2024-10-02 13:40:27 -0600

    [FEAT] Multiples cambios en mi archivo MyIcon
    - importando useTheme
    - importando la interface MyIconProps
    - importando StyleSheet
    - desestructurando las props name,color y white y tipandolas por la interface MyIconProps
    - hacemos una validación para el color correspondiente al establecimiento de la prop white ó color
    - estableciendo las props name, color y style en el component Icon
    - creando el objeto styles el cual tiene los estilos del icono

--- src/components/ui/MyIcon.tsx
+++ src/components/ui/MyIcon.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
-import { Icon } from '@ui-kitten/components';
+import { Icon,useTheme } from '@ui-kitten/components';
+import { MyIconProps } from '../../types/Interfaces';
+import { StyleSheet } from 'react-native';
 
-export const MyIcon = () => {
+export const MyIcon = ({name,color,white}:MyIconProps) => {
+    const theme = useTheme();
+
+    if (white) {
+        color = theme['color-info-100'];
+    } else if (!color){
+        color = theme['text-basic-color'];
+    }else {
+        color = theme[color];
+    }
   return (
-    <Icon/>
+    <Icon name={name} fill={color} style={styles.icon}/>
   );
 };
+
+const styles = StyleSheet.create({
+    icon:{
+        width:32,
+        height:32,
+    },
+});
