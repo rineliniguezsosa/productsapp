52e0d5b614322c79508be59c2a33f798d42b0dc1
Rinel <rineliniguezsosa@gmail.com>
2024-10-02 17:32:41 -0600

    [FEAT] Estableciendo un punto y coma en el la variable API_URL_IOS de nuestro tipado de las variables en el archivo env.d.ts

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -3,5 +3,5 @@ declare module '@env' {
     export const STAGE: string;
     export const API_URL: string;
     export const API_URL_ANDROID: string;
-    export const API_URL_IOS: string
+    export const API_URL_IOS: string;
 }
