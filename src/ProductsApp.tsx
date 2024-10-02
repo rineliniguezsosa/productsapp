79f83c3efa49ecfdc86ffee8d1e37de76ce40a88
Rinel <rineliniguezsosa@gmail.com>
2024-10-01 21:08:39 -0600

    [FEAT] Cambios en el ProductApp.tsx
    - importando ApplicationProvider,IconRegistry
    - importando EvaIconsPack
    - envolviendo el component ProductsApp por un fragment
    - estableciendo el componente IconRegistry  con la prop icons y con el set de icons de EvaIconsPack

--- src/ProductsApp.tsx
+++ src/ProductsApp.tsx
@@ -3,14 +3,18 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './navigation/StackNavigator';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider } from '@ui-kitten/components';
+import { ApplicationProvider,IconRegistry } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
 export const ProductsApp = () => {
   return (
-    <ApplicationProvider  {...eva} theme={eva.light}>
-      <NavigationContainer>
-        <StackNavigator/>
-      </NavigationContainer>
-    </ApplicationProvider>
+      <>
+        <IconRegistry icons={EvaIconsPack} />
+        <ApplicationProvider  {...eva} theme={eva.light}>
+          <NavigationContainer>
+            <StackNavigator/>
+          </NavigationContainer>
+        </ApplicationProvider>
+      </>
   );
 };
