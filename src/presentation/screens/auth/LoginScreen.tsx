4243c118e3e720920d99334e31a4b7c6ddaf3af4
Rinel <rineliniguezsosa@gmail.com>
2024-10-02 14:26:11 -0600

    [FEAT] Cambios en el archivo LoginScreen
    - cambiando el tipo de tipografia del elemento Text de la linea 19 de "p" a "label"
    - estableciendo un log del height
    - estableciendo un solo Text para el titulo de "¿No tienes cuenta?"
    - eatableciendo otro elemento Text para el titulo de Registrate

--- src/presentation/screens/auth/LoginScreen.tsx
+++ src/presentation/screens/auth/LoginScreen.tsx
@@ -10,13 +10,15 @@ interface Props extends StackScreenProps<RootStackParams,'LoginScreen'>{}
 
 export const LoginScreen = ({navigation}:Props) => {
   const { height } = useWindowDimensions();
+  console.log(height);
+
   return (
     <Layout style={{flex:1}}>
       <ScrollView style={{marginHorizontal:40}}>
 
         <Layout style={{paddingTop:height * 0.35}} >
           <Text category="h1">Ingresar</Text>
-          <Text category="p">Porfavor,Ingrese para continuar</Text>
+          <Text category="label">Porfavor,Ingrese para continuar</Text>
         </Layout>
 
         <Layout style={{marginTop:20}}>
@@ -45,7 +47,8 @@ export const LoginScreen = ({navigation}:Props) => {
         </Layout>
 
         <Layout style={{marginTop:20,flexDirection:'row',justifyContent:'center',alignItems:'flex-end'}}>
-          <Text status="primary" category="s1" onPress={()=> navigation.navigate('RegisterScreen')}>¿No tienes cuenta?</Text>
+          <Text>¿No tienes cuenta?</Text>
+          <Text status="primary" category="s1" onPress={()=> navigation.navigate('RegisterScreen')}>Registrate</Text>
         </Layout>
 
       </ScrollView>
