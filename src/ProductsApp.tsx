7774eddedb3254e423401593cc63136249fd029d
Rinel <rineliniguezsosa@gmail.com>
2024-10-01 20:58:05 -0600

    [FEAT] Cambios en el archivo ProductApp para establecer el provider de kitten
    - importando todo de eva
    - importando ApplicationProvider
    - envolviendo el contenido de ProductsApp por el provider de ApplicationProvider de ui-kitten
     - pasando las props al   ApplicationProvider

--- src/ProductsApp.tsx
+++ src/ProductsApp.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './navigation/StackNavigator';
-
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider } from '@ui-kitten/components';
 
 export const ProductsApp = () => {
   return (
-    <NavigationContainer>
-      <StackNavigator/>
-    </NavigationContainer>
+    <ApplicationProvider  {...eva} theme={eva.light}>
+      <NavigationContainer>
+        <StackNavigator/>
+      </NavigationContainer>
+    </ApplicationProvider>
   );
 };
