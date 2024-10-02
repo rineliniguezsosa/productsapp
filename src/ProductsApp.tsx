afd3e748ce1896b93d0cc59a77d753a7d26e24cd
Rinel <rineliniguezsosa@gmail.com>
2024-10-01 20:46:01 -0600

    [FEAT] Cambios en el component ProductsApp
    - importando el StackNavigator
    - estableciendo en el retorno del component roductsApp  el component StackNavigator para definir la navegación del stack

--- src/ProductsApp.tsx
+++ src/ProductsApp.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
+import { StackNavigator } from './navigation/StackNavigator';
+
 
 export const ProductsApp = () => {
   return (
     <NavigationContainer>
-
+      <StackNavigator/>
     </NavigationContainer>
   );
 };
