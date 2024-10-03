e6ee15f800c0e07e225bd4847ae4fd822eeb2070
Rinel <rineliniguezsosa@gmail.com>
2024-10-02 18:20:04 -0600

    [FEAT] Cambios en el archivo auth.ts
    - impprtando la interface User
    - creando una función llamada userAuth para dar formato a la respuesta del servidor, creé el objeto user tipado con la interface de User donde estableci la data de solo el usuario para separar la info del usuario con la del token
    - estableci un return donde devolvi un objeto pasando el user y el token
    - cambiando en el retorno del método authLogin por la función userAuth donde le pasamos todo la data a la función para poder separar después la información

--- src/actions/auth/auth.ts
+++ src/actions/auth/auth.ts
@@ -1,11 +1,26 @@
 import { Authresponse } from './../../infrastructure/auth.response';
 import { tesloApi } from '../../config/api/TesloApi';
+import { User } from '../../Interfaces/Interfaces';
 
+const userAuth = (data:Authresponse) => {
+    const user:User = {
+        id:data.id,
+        email:data.email,
+        fullName:data.fullName,
+        isActive:data.isActive,
+        roles:data.roles,
+    };
+
+    return {
+        user:user,
+        token:data.token,
+    };
+};
 export const authLogin = async(email:string,password:string) =>{
     try {
         const { data } = await tesloApi.post<Authresponse>('/auth/login',{email,password});
 
-        return data;
+        return userAuth(data);
     } catch (error) {
         console.log(error);
         return null;
