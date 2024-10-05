683621430a426d81d8b492d5ae6be981f0b52e64
Rinel <rineliniguezsosa@gmail.com>
2024-10-04 19:45:44 -0600

    [FEAT] Guardando el funcional component AuthProvider, este componente se encarga de la verificación de las rutas

---
+++ src/providers/AuthProvider.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import React,{ PropsWithChildren,useEffect } from 'react';
+import { RootStackParams } from '../Interfaces/Interfaces';
+import { useAuthStore } from '../store/auth/AuthStore';
+
+export const AuthProvider = ({children}:PropsWithChildren) => {
+    const navigation = useNavigation<StackNavigationProp<RootStackParams>>();
+    const { status,checkStatus } = useAuthStore();
+
+    useEffect(()=>{
+        checkStatus();
+    },[]);
+
+    useEffect(()=>{
+        if(status !== 'check'){
+            if (status === 'authenticated') {
+                navigation.reset({index:0,routes:[{name:'HomeScreen'}]});
+            }
+        }else{
+            navigation.reset({index:0,routes:[{name:'LoginScreen'}]});
+        }
+    },[status]);
+
+  return (
+    <>
+        {children}
+    </>
+  );
+};
