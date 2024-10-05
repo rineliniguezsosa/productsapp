b05015025325848164b04e8dc20b5a5c590c79b5
Rinel <rineliniguezsosa@gmail.com>
2024-10-04 19:59:14 -0600

    [FEAT] Cambios en el HomeScreen
    - importando el store useAuthStore
    - desestructurando el método logout del useAuthStore
    - estableciendo en el botón las props onPress donde pase la referencia del método logout y cambie el nombre del icono por "log-out-outline"

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,13 +1,15 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import { Layout,Text,Button,Icon } from '@ui-kitten/components';
+import { useAuthStore } from '../../../store/auth/AuthStore';
 
 export const HomeScreen = () => {
+  const { logout } = useAuthStore();
   return (
     <Layout style={{flex:1,justifyContent:'center',alignItems:'center'}}>
       <Text>HomeScreen</Text>
 
-      <Button accessoryLeft={<Icon name="facebook"/>}>Hola</Button>
+      <Button onPress={logout} accessoryLeft={<Icon name="log-out-outline"/>}>Cerrar sesión</Button>
     </Layout>
   );
 };
