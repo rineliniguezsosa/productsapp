c42ff334b9cda0e147096c18848aec8835b37db5
Rinel <rineliniguezsosa@gmail.com>
2024-10-04 19:05:13 -0600

    [FEAT] Creando un interceptor para actualizar el token del usuario
    - por cada request voy a asignarle un nuevo token
    - 1ero verificamos si tenemos un token y si existen lo establecemos en los headers

--- src/config/api/TesloApi.ts
+++ src/config/api/TesloApi.ts
@@ -1,10 +1,24 @@
 import { STAGE,API_URL as PROD_URL,API_URL_ANDROID,API_URL_IOS } from '@env';
 import axios from 'axios';
 import { Platform } from 'react-native';
+import { StorageAdapter } from '../../helpers/async.storage';
 
 const API_URL = (STAGE === 'prod') ? PROD_URL : Platform.OS === 'ios' ? API_URL_IOS : API_URL_ANDROID;
 
 const tesloApi = axios.create({baseURL:API_URL,headers:{'Content-Type':'application/json'}});
+
+//interceptors
+//if I have a token, updated
+tesloApi.interceptors.request.use(
+    async(config)=>{
+        const token = await StorageAdapter.getItem('token');
+
+        if(token){
+            config.headers.Authorization = `Barer ${token}`;
+        }
+        return config;
+    }
+);
 
 export {
     tesloApi,
