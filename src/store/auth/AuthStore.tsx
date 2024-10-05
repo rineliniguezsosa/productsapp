01ecaf04765ecfa18a4af7728ce96d0d339729f0
Rinel <rineliniguezsosa@gmail.com>
2024-10-04 19:12:39 -0600

    [FEAT] Cambios en el archivo AuthSore
    - importando la acción authCheckStatus para realizar la solicitud http
    - establecemos la nueva prop checkStatus en la interface AuthState donde devolvemos una función con una promesa vacia
    - establecemos la función checkStatus en el store useAuthStore para poder usar la acción  y copiamos casi lo mismo del método login

--- src/store/auth/AuthStore.tsx
+++ src/store/auth/AuthStore.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { create } from 'zustand';
 import { User } from '../../Interfaces/Interfaces';
-import { authLogin } from '../../actions/auth/auth';
+import { authCheckStatus, authLogin } from '../../actions/auth/auth';
 import { StorageAdapter } from '../../helpers/async.storage';
 
 type AuthStatusTypes = 'check' | 'notauthenticated' | 'authenticated';
@@ -10,7 +10,8 @@ interface AuthState {
     status: AuthStatusTypes,
     token?:string,
     user?:User,
-    login:(email:string,password:string)=> Promise<boolean>
+    login:(email:string,password:string)=> Promise<boolean>,
+    checkStatus:() => Promise<void>
 }
 
 
@@ -32,5 +33,19 @@ export const useAuthStore = create<AuthState>()((set,get)=>({
 
         set({status:'authenticated',token:resp.token,user:resp.user});
         return true;
+    },
+    checkStatus:async() =>{
+        const resp = await authCheckStatus();
+
+        if (!resp) {
+            set({status:'authenticated',token:undefined,user:undefined});
+            return;
+        }
+
+        //save token in localStorage
+        await StorageAdapter.setItem('token',resp.token);
+
+        set({status:'authenticated',token:resp.token,user:resp.user});
+
     },
 }));
